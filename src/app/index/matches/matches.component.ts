a3d306413149d55339dba2a76e599899da19dd0d
Ravidevarasu <ravi.devarasu@twilightsoftwares.com>
2022-03-02 10:12:26 +0530

    initial commit

---
+++ src/app/index/matches/matches.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matches',
+  templateUrl: './matches.component.html',
+  styleUrls: ['./matches.component.scss'],
+})
+export class MatchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
